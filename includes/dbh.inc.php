581c4cf1f221f2a3a674f2eb6fb41d29831e0c39
lahiru1115 <lahirudissanayake15@gmail.com>
2022-12-15 18:27:22 +0530

    DB Update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "e-fine";
+$dBName = "e-fine-lahiru";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
